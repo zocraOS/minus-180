b0bf3c7486fdc978435512b98d4043b189cd3853
zocraOS <pascaljbarth@web.de>
2020-03-27 18:07:33 +0100

    Initial Commit

---
+++ Minus180/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minus180
+//
+//  Created by Pascal Barth on 27.03.20.
+//  Copyright © 2020 zocraOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
